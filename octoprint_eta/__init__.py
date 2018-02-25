d20fa3b93f4aeb24f407443dde485c9fdac8d31b
Marcos Teixeira de Mello <mtmello2003@yahoo.com.br>
2018-02-25 19:17:23 -0300

    Add files via upload
    Optimized message.
    Removed useless long messages .

--- octoprint_eta/__init__.py
+++ octoprint_eta/__init__.py
@@ -1,87 +1,150 @@
 # coding=utf-8
+
+## Adjusts Marcos T Mello - Fev 2018
+##
+
 from __future__ import absolute_import
+import time
+import socket
 
 import octoprint.plugin
-from octoprint.util import RepeatedTimer
-import time
-import datetime
-from babel.dates import format_date, format_datetime, format_time
+import octoprint.util
+import traceback
+from octoprint.events import Events
 
-class DisplayETAPlugin(octoprint.plugin.ProgressPlugin,
-                       octoprint.plugin.TemplatePlugin,
-                       octoprint.plugin.AssetPlugin,
-                       octoprint.plugin.EventHandlerPlugin):
+class DetailedProgressPlugin(octoprint.plugin.EventHandlerPlugin,
+                             octoprint.plugin.SettingsPlugin):
+	_last_updated = 0.0
+	_last_message = 0
+	_repeat_timer = None
+	_etl_format = ""
+	_eta_strftime = ""
+	_messages = []
+	def on_event(self, event, payload):
+		if event == Events.PRINT_STARTED:
+			self._logger.info("Printing started. Detailed progress started.")
+			self._etl_format = self._settings.get(["etl_format"])
+			self._eta_strftime = self._settings.get(["eta_strftime"])
+			self._messages = self._settings.get(["messages"])
+			self._repeat_timer = octoprint.util.RepeatedTimer(self._settings.get_int(["time_to_change"]), self.do_work)
+			self._repeat_timer.start()
+		elif event in (Events.PRINT_DONE, Events.PRINT_FAILED, Events.PRINT_CANCELLED):
+			if self._repeat_timer != None:
+				self._repeat_timer.cancel()
+				self._repeat_timer = None
+			self._logger.info("Printing stopped. Detailed progress stopped.")
+			self._printer.commands("M117 Print Done")
+		elif event == Events.CONNECTED:
+			ip = self._get_host_ip()
+			if not ip:
+				return
+			self._printer.commands("M117 IP {}".format(ip))
 
-    def __init__(self):
-        self.eta_string = "-"
-        self.timer = RepeatedTimer(15.0, DisplayETAPlugin.fromTimer, args=[self], run_first=True,)
+	def do_work(self):
+		if not self._printer.is_printing():
+			#we have nothing to do here
+			return
+		try:
+			currentData = self._printer.get_current_data()
+			currentData = self._sanitize_current_data(currentData)
 
-    def fromTimer(self):
-        self.eta_string = self.calculate_ETA()
-        self._plugin_manager.send_plugin_message(self._identifier, dict(eta_string=self.eta_string))
-        
-        
-    def calculate_ETA(self):
-        currentData = self._printer.get_current_data()
-        if not currentData["progress"]["printTimeLeft"]:
-            return "-"
-        current_time = datetime.datetime.today()
-        finish_time = current_time + datetime.timedelta(0,currentData["progress"]["printTimeLeft"])
-        strtime = format_time(finish_time)
-        strdate = ""
-        if finish_time.day > current_time.day:
-            if finish_time.day == current_time.day + 1:
-                strdate = " Tomorrow"
-            else:
-                strtime = " " + format_date(finish_time,"EEE d")
-        return strtime + strdate
-            
-        
-        
-    def on_print_progress(self,storage, path, progress):
-        self.eta_string = self.calculate_ETA()
-        self._plugin_manager.send_plugin_message(self._identifier, dict(eta_string=self.eta_string))
-        
-    def on_event(self,event, payload):
-        if event.startswith('Print'):
-            if event not in {"PrintStarted","PrintResumed"}:
-                self.eta_string="-"
-                self.timer.cancel()
-            else:
-                self.eta_string = self.calculate_ETA()
-                self.timer.cancel()
-                self.timer = RepeatedTimer(10.0, DisplayETAPlugin.fromTimer, args=[self], run_first=True,)
-                self.timer.start()
-            self._plugin_manager.send_plugin_message(self._identifier, dict(eta_string=self.eta_string))
-            
-    def get_assets(self):
-        return {
-            "js": ["js/displayeta.js"]
-        } 
+			message = self._get_next_message(currentData)
+			self._printer.commands("M117 {}".format(message))
+		except Exception as e:
+			self._logger.info("Caught an exception {0}\nTraceback:{1}".format(e,traceback.format_exc()))
+
+	def _sanitize_current_data(self, currentData):
+		if (currentData["progress"]["printTimeLeft"] == None):
+			currentData["progress"]["printTimeLeft"] = currentData["job"]["estimatedPrintTime"]
+		if (currentData["progress"]["filepos"] == None):
+			currentData["progress"]["filepos"] = 0
+		if (currentData["progress"]["printTime"] == None):
+			currentData["progress"]["printTime"] = currentData["job"]["estimatedPrintTime"]
+
+		currentData["progress"]["printTimeLeftString"] = "Not yet"
+		currentData["progress"]["ETA"] = "Not yet"
+
+		#Add additional data
+		try:
+			currentData["progress"]["printTimeLeftString"] = self._get_time_from_seconds(currentData["progress"]["printTimeLeft"])
+			currentData["progress"]["ETA"] = time.strftime(self._eta_strftime, time.localtime(time.time() + currentData["progress"]["printTimeLeft"]))
+		except Exception as e:
+			self._logger.debug("Caught an exception trying to parse data: {0}\n Error is: {1}\nTraceback:{2}".format(currentData,e,traceback.format_exc()))
+
+		return currentData
+
+	def _get_next_message(self, currentData):
+		message = self._messages[self._last_message]
+		self._last_message += 1
+		if (self._last_message >= len(self._messages)):
+			self._last_message = 0
+		return message.format(
+			completion = currentData["progress"]["completion"],
+			printTimeLeft = currentData["progress"]["printTimeLeftString"],
+			ETA = currentData["progress"]["ETA"],
+			filepos = currentData["progress"]["filepos"]
+		)
+
+	def _get_time_from_seconds(self, seconds):
+		hours = 0
+		minutes = 0
+		if seconds >= 3600:
+			hours = int(seconds / 3600)
+			seconds = seconds % 3600
+		if seconds >= 60:
+			minutes = int(seconds / 60)
+			seconds = seconds % 60
+		return self._etl_format.format(**locals())
+
+	def _get_host_ip(self):
+		return [l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0]
+
+	##~~ Settings
+
+	def get_settings_defaults(self):
+		return dict(
+			messages = [
+				"{completion:.1f}%  ETL {printTimeLeft}"
+				##, 
+				##"ETL {printTimeLeft}" 
+				##,
+				##"ETA {ETA}"
+				## REMOVED INFORMATION USELESS
+			],
+			eta_strftime = "%H %M Day %d",
+			etl_format = "{hours:02d}h{minutes:02d}m",
+			time_to_change = 10
+		)
+
+	##~~ Softwareupdate hook
+
+	def get_update_information(self):
+		return dict(
+			detailedprogress=dict(
+				displayName="DetailedProgress Plugin",
+				displayVersion=self._plugin_version,
+
+				# version check: github repository
+				type="github_release",
+				user="dattas",
+				repo="OctoPrint-DetailedProgress",
+				current=self._plugin_version,
+
+				# update method: pip
+				pip="https://github.com/dattas/OctoPrint-DetailedProgress/archive/{target_version}.zip"
+			)
+		)
+
+__plugin_name__ = "Detailed Progress Plugin"
+
+def __plugin_load__():
+	global __plugin_implementation__
+	__plugin_implementation__ = DetailedProgressPlugin()
+
+	global __plugin_hooks__
+	__plugin_hooks__ = {
+		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information,
+	}
 
 
-    def get_update_information(self):
-        return {
-            "display-eta":dict(
-                displayName=self._plugin_name,
-                displayVersion=self._plugin_version,
-
-                type="github_release",
-                current=self._plugin_version,
-                user="pablogventura",
-                repo="Octoprint-ETA",
-
-                pip="https://github.com/pablogventura/Octoprint-ETA/archive/{target}.zip"
-                )
-            }
-
-__plugin_name__ = "Display ETA"
-__plugin_identifier = "display-eta"
-__plugin_version__ = "1.0.2"
-__plugin_description__ = "Show finish time (ETA time) for current print."
-__plugin_implementation__ = DisplayETAPlugin()
-
-__plugin_hooks__ = {
-    "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
-}
-
+	
